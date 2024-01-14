d7eae74abbfdf3298eb89014e502d0bee54779f1
jooanna4 <joana.bruguera@gmail.com>
2024-01-14 17:50:39 +0100

    ff

--- dialog.cpp
+++ dialog.cpp
@@ -1,18 +1,19 @@
 #include "dialog.hpp"
+#include "phone.hpp"
 
 void dialog::dialog(easy_dial& easy, const string& input, vector<string>& answers, nat& numtelf) throw() {
     nat i = 0;
     answers.push_back(easy.inici());
-    for (i = 0; i < input.length(); ++i) {
-      if (input[i] == '<') {
+    for (i = 0; i < input.size(); i++) {
+      if (input[i] == phone::DELETECHAR) 
         answers.push_back(easy.anterior());
-      }
-      else if(input[i] == '|') {
+
+      else if(input[i] == phone::ENDCHAR) 
         answers.push_back(easy.seguent('\0'));
-      }
-      else {
+      
+      else 
         answers.push_back(easy.seguent(input[i]));
-      }
+      
     }
     numtelf = easy.num_telf();
 }
